bb8e7d47d1b817459efeb589497c9fc4c84b35fe
EricWeisfeld <ericweisfeld@gmail.com>
2023-01-29 14:00:03 -0500

    General Formating and test

---
+++ Assets/Scripts/Fleet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fleet : MonoBehaviour
+{
+
+    public int NumOfStarFighters;
+    public int NumOfCapitalShips;
+    //public StarFighter starFighter;
+    //public CapitalShip capitalShip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
