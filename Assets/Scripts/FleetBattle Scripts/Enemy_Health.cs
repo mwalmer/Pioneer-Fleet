6d6d20c5c6436bcc620f21664731e46f9c116da8
Yifan Zhu <zhujimbo@gmail.com>
2023-02-05 18:56:01 -0500

    prototype starfighter scene

---
+++ Assets/Scripts/FleetBattle Scripts/Enemy_Health.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Health : MonoBehaviour {
+
+	void Update () {
+		if (gameObject.transform.position.y < -10) {
+            Destroy (gameObject);
+        }
+	}
+}
