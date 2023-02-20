cfab2b44340817d141bf1333c00586f7a67ff00a
Yifan Zhu <zhujimbo@gmail.com>
2023-02-20 15:39:15 -0500

    torpedo finished

--- Assets/torpedo.cs
+++ Assets/torpedo.cs
@@ -2,45 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+
 public class torpedo : MonoBehaviour {
 
-	public float velocity = 20f;
-    public float acceleration = 20f;
+	public float velocity = 10f;
+    public float eventualspeed = 60f;
+	public float accTime = 3f;
 	public int damage = 40;
 //	public Rigidbody2D rb;
 	public GameObject impactEffect;
 	public float angle;
+	float currentV;
+
+	float currentY;
+	public float accTY=1f;
+	public float eY=0f;
+	public float veloY=10f;
+	// Rigidbody m_Rigidbody;
 	// Use this for initialization
 	void Start () {
-	
+	 currentV=velocity;
+	 currentY=veloY;
 	}
 	void Update()
 	{
 		Vector3 newPos = transform.position;
-		newPos.x += (velocity +acceleration* Time.deltaTime*10)* Time.deltaTime;
+		currentV=Mathf.Lerp(currentV,eventualspeed,accTime*Time.deltaTime);
+		newPos.x += currentV* Time.deltaTime;
 		//(float)transform.rotation.y*(float).5
-		newPos.y += velocity * Time.deltaTime*angle ;
+		currentY=Mathf.Lerp(currentY,eY,accTY*Time.deltaTime);
+		newPos.y += currentY* Time.deltaTime;
 		transform.position = newPos;
-		
+				Debug.Log(currentY);
+	//	 m_Rigidbody.AddForce(transform.up * acceleration);
 
 	}
 	void OnTriggerEnter2D (Collider2D hitInfo)
 	{
+		
 		Enemy enemy = hitInfo.GetComponent<Enemy>();
-		if (enemy != null)
+		if(enemy.gameObject.layer==LayerMask.NameToLayer("Enemy"))
 		{
+			if (enemy != null)
+			{
 			enemy.TakeDamage(damage);
-		}
+			}
 
-		Instantiate(impactEffect, transform.position, transform.rotation);
-		foreach (Transform child in transform)
-		{
+			Instantiate(impactEffect, transform.position, transform.rotation);
+			foreach (Transform child in transform)
+			{
 			//Debug.Log("TEST" + child.gameObject.name);
-			Destroy(child.gameObject);
-		}
+				Destroy(child.gameObject);
+			}
 	//	Debug.Log("TEST2" + gameObject.name);
-		Destroy(gameObject);
-		//Debug.Log("TEST3");
+			Destroy(gameObject);
+		
+		}
+		
 	}
 	
 }
