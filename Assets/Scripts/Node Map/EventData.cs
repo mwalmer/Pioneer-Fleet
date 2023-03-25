2d14b8fe1447045e218e341f79bcbc9a984a09bb
mwalmer <75793394+mwalmer@users.noreply.github.com>
2023-03-25 17:40:48 -0400

    added a temp template for the event system. Subject to change

---
+++ Assets/Scripts/Node Map/EventData.cs
@@ -0,0 +1,20 @@
+public static class EventData
+{
+    // star fighter
+    // give range for each value 
+    
+    public static float difficultyMultiplier = 1.0f;
+    public static float healthMultiplier = 1.0f;
+    public static float speedMultiplier = 1.0f;
+    public static float projectileSpeedMultiplier = 1.0f;
+    public static int numSmallEnemies = 1;
+    public static int bigEnemies = 1;
+    public static int numWaves = 1; // example min = 1, max = 7
+
+
+    // flak cannon
+
+    // candy crush
+
+    // other stuff
+}
