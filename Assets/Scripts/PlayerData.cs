ec9843444a8d77d17ad197b56906485de9a12c7f
Eric <ericweisfeld@gmail.com>
2023-04-13 14:28:11 -0400

    Test

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -18,6 +18,7 @@ public class PlayerData : MonoBehaviour
     public string FleetLog;
 
 
+
     void Start()
     {
         currency = 150;
@@ -32,7 +33,7 @@ public class PlayerData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //test
     }
 
     public void addToFleetLog(string s){
